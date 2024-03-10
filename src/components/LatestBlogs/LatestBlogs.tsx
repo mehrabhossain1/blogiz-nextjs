1ee2a2bc48e5c93e81592c62e13a47251ff1426c
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-10 18:54:30 +0600

    latest blogs design

---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LatestBlogs = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center my-5">
+        Latest Blogs From <span className="text-accent">Blogiz</span>
+      </h1>
+      <p className="text-xl text-center text-gray-400 w-2/4 mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+    </div>
+  );
+};
+
+export default LatestBlogs;
