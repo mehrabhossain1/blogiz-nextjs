747dd15998b1e606cc787b985420fcd2a3b994df
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-10 19:22:11 +0600

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 
 const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  console.log(blogs);
   return (
     <>
-      <LatestBlogs />
+      <LatestBlogs blogs={blogs} />
     </>
   );
 };
--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,8 +1,11 @@
+import { Blog } from "@/types";
 import React from "react";
+import LatestBlogCard from "../ui/LatestBlogCard";
+import BlogCard from "../ui/BlogCard";
 
-const LatestBlogs = () => {
+const LatestBlogs = ({ blogs }: { blogs: Blog[] }) => {
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center my-5">
         Latest Blogs From <span className="text-accent">Blogiz</span>
       </h1>
@@ -12,6 +15,18 @@ const LatestBlogs = () => {
           unprecedented computational power.
         </i>
       </p>
+      {/* Mapped latest blogs card */}
+      <div className="grid grid-cols-2 gap-4 my-5">
+        {blogs.slice(0, 2).map((blog) => (
+          <LatestBlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 my-5">
+        {blogs.slice(2, 5).map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
     </div>
   );
 };
