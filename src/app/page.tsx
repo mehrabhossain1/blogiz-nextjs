4f4abc8245472f5b53b892556c704ac5d331c1c1
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-03-11 07:23:27 +0600

    data fetched.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/blogs");
+  const res = await fetch("http://localhost:5000/blogs", {
+    cache: "no-store",
+  });
+
   const blogs = await res.json();
-  console.log(blogs);
+  // console.log(blogs);
+
   return (
     <>
       <LatestBlogs blogs={blogs} />
